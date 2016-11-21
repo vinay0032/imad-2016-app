9d3c9c3c2274c3ffa43600f50f9b3b7404eb8be2
Vinay K P <vinay.p@aricent.com>
2016-11-21 18:41:39 +0530

    latest code4

--- server.js
+++ server.js
@@ -9,17 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('ui/bootstrap/css/bootstrap.min.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/bootstrap/css', 'bootstrap.min.css'));
-});
+app.use('/ui', express.static(path.join(__dirname, 'ui')));
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
