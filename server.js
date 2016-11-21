546103c2af2a0277288a82becb0dfd486d6eb49d
Vinay K P <vinay.p@aricent.com>
2016-11-21 20:26:57 +0530

    latest code6

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.use('/ui', express.static(path.join(__dirname, 'ui')));
+app.use(express.static(path.join(__dirname, 'ui')));
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
